f4178b8afdc21577ee799b189dfec577f83c27a6
Arilson <arilsontere18@hotmail.com>
2023-01-05 21:17:10 -0300

    post e comentario

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,19 +6,35 @@ export function Post() {
             <header>
                 <div className={styles.author}>
                     <img className={styles.avatar} src="https://github.com/Arilson1.png" />
-                </div>
-                <div className='authorInfo'>
-                    <strong>Arilson Florencio</strong>
-                    <span>Web Developer</span>
+                    <div className={styles.authorInfo}>
+                        <strong>Arilson Florencio</strong>
+                        <span>Web Developer</span>
+                    </div>
                 </div>
                 <time title='30 de Dezembro às 08:13h' dateTime="2022-30-12 08:13:30" >Publicado há 1h</time>
             </header>
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p> 
+                <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 jane.design/doctorcare</p>
-                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+                <p><a href='#'>👉 jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a href="#">#nlw</a>{' '}
+                    <a href="#">#rocketseat</a>
+                </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedBack</strong>
+
+                <textarea 
+                    placeholder='Deixe um comentário'
+                />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
         </article>
     );
 }
